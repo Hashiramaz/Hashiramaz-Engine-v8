8e5e3331ab6cf1d972cbee3b87fb669f268c5359
Vinni Hashirama <vinicius_freires@hotmail.com>
2021-05-28 18:29:54 -0300

    Add Terrain Spawner Correction with Groups

--- Assets/_Project/Scripts/WIP/TerrainSpawner.cs
+++ Assets/_Project/Scripts/WIP/TerrainSpawner.cs
@@ -6,25 +6,71 @@ public class TerrainSpawner : MonoBehaviourSingleton<TerrainSpawner>
 {
     public List<MoveTerrain> terrainsPrefab;
 
-
+    public bool pickFromTerrainGroup;
     public void SpawnNewTerrain()
     {
-        var terrainToSpawn = terrainsPrefab.PickRandomOne();
+        var terrainToSpawn = GetTerrainToSpawn();
 
-        Instantiate(terrainToSpawn,transform.position,transform.rotation);
+        Instantiate(terrainToSpawn, transform.position, transform.rotation);
     }
+
+
+    public MoveTerrain GetTerrainToSpawn()
+    {
+        MoveTerrain terrain = new MoveTerrain();
+
+        if(pickFromTerrainGroup && activeTerrainGroups.Count > 0)
+        {
+            int terrainGroupIndex = GetRandomTerrainGroupIndex();
+            terrain = terrainGroups[terrainGroupIndex].GetRandomTerrain();
+        }
+        else
+        {
+            terrain = terrainsPrefab.PickRandomOne();
+        }
+
+        return terrain;
+    }
+
 
     public List<TerrainGroup> terrainGroups;
 
     public List<int> activeTerrainGroups;
+
+    public void ActivateTerrainGroup(int groupIndex)
+    {
+        if(!activeTerrainGroups.Contains(groupIndex))
+        {
+            activeTerrainGroups.Add(groupIndex);
+        }
+    }
+
+    public void RemoveTerrainGroup(int groupIndex)
+    {
+        if(activeTerrainGroups.Contains(groupIndex))
+        {
+            activeTerrainGroups.Remove(groupIndex);
+        }
+    }
+
+    public int GetRandomTerrainGroupIndex()
+    {
+        return activeTerrainGroups.PickRandomOne();
+    }
+
+    public TerrainGroup GetTerrainGroup(int groupIndex)
+    {
+        return terrainGroups[groupIndex];
+    }
 }
 
 [System.Serializable]
-public class TerrainGroup{
-public List<MoveTerrain> terrainsPrefab;
-
-public MoveTerrain GetRandomTerrain()
+public class TerrainGroup
 {
-    return terrainsPrefab.PickRandomOne();
-}
+    public List<MoveTerrain> terrainsPrefab;
+
+    public MoveTerrain GetRandomTerrain()
+    {
+        return terrainsPrefab.PickRandomOne();
+    }
 }
