90e5cd4635f1a83118814f1df505a3f8cf6d9d6a
Vinni <vinicius_freires@hotmail.com>
2022-08-16 06:02:20 -0300

    Organize assets

---
+++ Assets/_Project/Scripts/AudioSource/AudioSourceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lasp;
+
+public class AudioSourceManager : MonoBehaviourSingleton<AudioSourceManager>
+{
+    public string selectedDeviceID;
+
+    public string[] avaiableDevices;
+
+    public AudioLevelTracker audioTracker;
+
+
+    private void Start() {
+        selectedDeviceID = audioTracker.deviceID;
+    }
+
+    public void SelectDevice()
+    {
+
+    }
+
+    public void OnDeviceSelected()
+    {
+
+    }
+}
